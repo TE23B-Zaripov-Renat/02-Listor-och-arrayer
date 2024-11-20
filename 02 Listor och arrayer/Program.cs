f58174d7da5220de1375334913521a2fa924b876
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-11-20 15:01:50 +0100

    rtnhrtyty

---
+++ 02 Listor och arrayer/Program.cs
@@ -0,0 +1,12 @@
+﻿// List<string> leksaker = ["YOYO", "Hotwheels, Lego, Barbie, Kalle Anka"];
+// -------------------------------------------------------------------------------------
+// List<string> leksaker = ["YOYO", "Hotwheels", "Lego", "Barbie", "Kalle Anka"];
+
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine(leksaker[i]);
+
+// }
+
+// Console.ReadLine();
+// --------------------------------------------------------------------------------------
