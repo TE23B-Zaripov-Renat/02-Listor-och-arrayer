0f1a8c7e6c20168f973f3c01bdfa4ee373528b41
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-11-27 15:04:51 +0100

    hbifsv

--- 02 Listor och arrayer/Program.cs
+++ 02 Listor och arrayer/Program.cs
@@ -21,7 +21,7 @@
 // Console.ReadLine();
 // --------------------------------------------------------------------------------------
 // List<int> numbers = [5, 8, 3, 4, 9];
- 
+
 //  for (int i = 0; i < 5; i++)
 // {
 //     Console.WriteLine(numbers[i]);
@@ -29,5 +29,45 @@
 // }
 
 
+
 // Console.ReadLine();
 // --------------------------------------------------------------------------------------
+
+List<int> antal = [2, 6, 7, 9, 1];
+
+List<string> namn = ["Benji", "Eddie", "Sandor", "David", "Kevin"];
+
+List<string> leksak = ["YOYO", "Hotwheels, Lego, Barbie, Kalle Anka"];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{namn[i]} ger {leksak[0]} betyget {antal[0]}");
+
+}
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+//  int[] betyg = { 5, 8, 3, 7, 10 };
+
+//         // Lista med klasskamraters namn
+//         string[] klasskamrater = { "Pelle", "Anna", "Johan", "Lisa", "Erik" };
+
+//         // Lista med leksakernas namn
+//         string[] leksaker = { "Barbie", "Legobit", "Boll", "Tåg", "Docka" };
+
+//         // Loopar igenom arrayen och skriver ut betyg
+//         for (int i = 0; i < betyg.Length; i++)
+//         {
+//             Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {betyg[i]}");
+//         }
+//         Console.ReadLine();
