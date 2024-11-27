7698cb15e13458168a847810c3adf10f2de4185b
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-11-27 19:10:20 +0100

    irejnfjev

--- 02 Listor och arrayer/Program.cs
+++ 02 Listor och arrayer/Program.cs
@@ -33,41 +33,17 @@
 // Console.ReadLine();
 // --------------------------------------------------------------------------------------
 
-List<int> antal = [2, 6, 7, 9, 1];
+// List<int> antal = [2, 6, 7, 9, 1];
 
-List<string> namn = ["Benji", "Eddie", "Sandor", "David", "Kevin"];
+// List<string> namn = ["Benji", "Eddie", "Sandor", "David", "Kevin"];
 
-List<string> leksak = ["YOYO", "Hotwheels, Lego, Barbie, Kalle Anka"];
+// List<string> leksak = ["YOYO", "Hotwheels", "Lego", "Barbie", "Kalle Anka"];
 
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"{namn[i]} ger {leksak[0]} betyget {antal[0]}");
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.WriteLine($"{namn[i]} ger {leksak[i]} betyget {antal[i]}");
 
-}
+// }
 
-Console.ReadLine();
+// Console.ReadLine();
 
-
-
-
-
-
-
-
-
-
-
-//  int[] betyg = { 5, 8, 3, 7, 10 };
-
-//         // Lista med klasskamraters namn
-//         string[] klasskamrater = { "Pelle", "Anna", "Johan", "Lisa", "Erik" };
-
-//         // Lista med leksakernas namn
-//         string[] leksaker = { "Barbie", "Legobit", "Boll", "Tåg", "Docka" };
-
-//         // Loopar igenom arrayen och skriver ut betyg
-//         for (int i = 0; i < betyg.Length; i++)
-//         {
-//             Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {betyg[i]}");
-//         }
-//         Console.ReadLine();
